b5256d06b4b9df065c1f9352662c315f6ae4bb14
wangpengpeng <pengpeng.wang@ishansong.com>
2019-04-16 20:10:56 +0800

    步骤条组件

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Steps from "./Steps";
+import styles from "./App.less";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={styles.app}>
+        <Steps
+          current={1}
+          source={[
+            { id: "1", title: "开始", description: "开始节点" },
+            { id: "2", title: "进行中", description: "开始节点" },
+            { id: "3", title: "未开始", description: "开始节点" }
+          ]}
+        />
       </div>
     );
   }
